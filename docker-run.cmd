452beebb0ed59ba12026ff1fea6f2b0eea24ecf6
sebagomez <sebastiangomezcorrea@gmail.com>
2020-08-17 16:34:41 -0300

    Added CS as current company and updated skills set

--- docker-run.cmd
+++
@@ -1,4 +0,0 @@
-docker run --rm -v c:/code/sebastian/Jalpc:/srv/jekyll -p 4000:4000 -it jekyll/builder:3 bash
-REM jekyll build
-REM jekyll serve
-REM more commands at: https://github.com/MilanAryal/CLI-Cheat-Sheet/blob/master/jekyll-commands.md
